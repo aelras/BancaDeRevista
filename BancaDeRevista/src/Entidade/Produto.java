7af08bc8a250a38f90c83f24d721d5629709bc7b
andre <andre@Andre>
2022-12-04 08:35:05 -0300

    Criando os Getters e Setters e criando método para incluir a quantidade disponível no estoque

--- BancaDeRevista/src/Entidade/Produto.java
+++ BancaDeRevista/src/Entidade/Produto.java
@@ -7,4 +7,45 @@ public class Produto {
 	private int estoque;
 	private double valorUnitario;
 	
+	public Produto (int codigo, String nome) {
+		
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public double getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setValorUnitario(double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	
+	//métodos
+	public void incluirEstoque(int quantidade) {
+		
+		estoque = estoque + quantidade;
+	}
+	
+	
 }
